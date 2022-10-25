5b978ff97ba0e1a6014b0de3fe8431977beaa0cb
LolTaun <91201899+LolTaun@users.noreply.github.com>
2022-10-25 20:05:21 +0300

    Rhombus Update
    Fixed rhombus

--- SSW_lab5/4.sh
+++ SSW_lab5/4.sh
@@ -1,77 +1,38 @@
 #!/bin/bash
 
-# Digital art bash script ;D
-# (Rhombus)
-
-if (( $1 < 3 ))
-then
-  echo "Size must be more than 2, while the current size is $1"
+# input check
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]]; then
+  echo "$1 is not a number"
   exit 1
 fi
-half=$(($1 / 2))
-is_odd=$(($1 % 2))
 
-if(($is_odd))
- then
-  for ((i = 1; i <= $1; i+=2))
-  do
-    margin=$(($1-$i))
-    for ((j = 0; j < $margin/2; j++))
-    do
-      echo -n " "
-    done
-    for((j = 0; j < $i; j++))
-    do
-      echo -n "."
-    done
-    for((j=$margin/2+1; j<=$margin; j++))
-    do
-      echo -n " "
-    done
-    echo " "
-  done
-  for (( i = $1 - 2; i > 0; i-=2 ))
-   do
-     margin=$(($1-$i))
-         for ((j = 0; j < $margin/2; j++))
-         do
-           echo -n " "
-         done
-         for((j = 0; j < $i; j++))
-         do
-           echo -n "."
-         done
-         for((j=$margin/2+1; j<=$margin; j++))
-         do
-           echo -n " "
-         done
-         echo ""
-   done
+# 1st loop to print the upper half of rhombus
+index=$(($1-2))
+for(( i = 0; i < $1; i++)); do
+	#first subloop to print margin
+	for(( j = 1; j < $index + 2; j++)); do
+		echo -n " "
+	done
 
-  else
-    for(( i = 1; i <= $1; i++))
-    do
-      for(( j = $half + 1; j <= $1; j++))
-      do
-        if (( $i <= $half ))
-        then
-          if (( $1 - $i >= $j ))
-          then
-            echo -n " "
-          else
-            echo -n ".."
-          fi
-        else
-          if(( $j <= $i ))
-          then
-            echo -n " "
-          else
-            echo -n ".."
-          fi
-        fi
-      done
-      echo ""
-    done
-fi
+	for(( j = 1; j < ($1 - $index); j++)); do
+		echo -n "x "
+	done
+	index=$(($index-1))
+	echo ""
+done
+
+# 2nd loop to print the lower half of rhombus
+index=2
+for(( i = $1-1; i > 0; i--)); do
+	for(( j = $index; j > 1; j--)); do
+		echo -n " "
+	done
+	for(( j = ($1 - $index + 2); j > 1; j--)); do
+		echo -n "x "
+	done
+	index=$(($index+1))
+	echo ""
+done
 
 exit 0
