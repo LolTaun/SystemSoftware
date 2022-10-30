338a0aa5c911873901910887e3132c720eaf9f8d
LolTaun <loltaun001@gmail.com>
2022-10-30 12:35:37 +0300

    task3 ready (with comments)

--- SSW_let2/task3.sh
+++ SSW_let2/task3.sh
@@ -1,21 +1,65 @@
 #!/bin/bash
+############ TASK3 [CODE PHRASES] ############
 
-declare -a array="( $1 )"
+#reading input into an array
+read -r -a array <<< $1
+#creating variable index to lowercase letters later
+index=0
 
-for elem in $array; do
+#---{START}-----------/ MAIN LOOP \-------------
+for elem in ${array[@]}; do
+  # creating word as an array of chars
   while read -n 1 c; do word+=($c); done <<< "$elem"
+
+  # creating an array that consists of indexes in ASCII table
   for(( i = 0; i < ${#word[@]}; i++ )); do
     code_word[$i]=$(printf "%d\n" "'${word[$i]}")
   done
 
+  # Main SubLoop to transform words:
+  for(( i = 0; i < ${#code_word[@]}; i++, index++)); do
+        #{START}============/if [CHAR] (ASCII index 65-90)\==================
+        if(( code_word[$i] > 64 && code_word[$i] < 91)); then
+          # shift by 1
+          code_word[$i]=$((${code_word[$i]}+1))
+          # if it goes beyond ASCII index 90 (circular shift)
+          if(( code_word[$i] > 90 )); then
+            code_word[$i]=$((${code_word[$i]}-26))
+          fi
 
-  for(( i = 0; i < ${#code_word[@]}; i++)); do
-    code_word[$i]=${code_word[$i]}-1
+          # to lowercase letters
+          if(( $index % 2 )); then
+            code_word[$i]=$((${code_word[$i]}+32))
+          fi
+        #{END}===============\if [CHAR] (ASCII index 65-90)/================
+
+        #{START}=============/if [INT] (ASCII index 48-57)\==================
+        elif((code_word[$i] > 47 && code_word[$i] < 58)); then
+          # decrease number by 9
+          code_word[$i]=$(( $(printf "%d\n" "'$((9-$(printf "\x$(printf %x ${code_word[$i]})")))") ))
+        fi
+        #{END}===============\if [INT] (ASCII index 48-57)/==================
+  done
+  # increasing index for next iterations
+  index=$(($index+1))
+
+  # Creating tmp_array which will become a modified word
+  for (( i = 0; i < ${#code_word[@]}; i++)); do
+    tmp_array+=$(printf "\x$(printf %x ${code_word[$i]})")
   done
 
-for (( i = 0; i < ${#code_word[@]}; i++)); do
-  echo ${code_word[$i]}
+  # add modified word to a global array
+  answer_arr+=("${tmp_array[@]}")
+
+  # destroy previously used in loop variables
+  unset word code_word tmp_array
 done
-done
+#---{END}-------------\ MAIN LOOP /-------------
+
+#transform array of modified word into a single string
+str=${answer_arr[@]}
+
+# use task1.sh to invert words and print output
+./task1.sh "$str"
 
 exit 0
