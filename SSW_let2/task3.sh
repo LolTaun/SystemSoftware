f25dc1a513235eadf733c3c601006bdfe7cf835c
LolTaun <91201899+LolTaun@users.noreply.github.com>
2022-11-02 14:11:51 +0300

    task3 update

--- SSW_let2/task3.sh
+++ SSW_let2/task3.sh
@@ -59,7 +59,22 @@ done
 #transform array of modified word into a single string
 str=${answer_arr[@]}
 
-# use task1.sh to invert words and print output
-./task1.sh "$str"
+# use task1.sh code to invert words and print output
+length=${#str}
+read -r -a array <<< $str
+length=${#array[@]}
+
+for(( i = 0; i < $length/2; i++)); do
+  tmp=${array[$length-$i-1]}
+  array[$length-$i-1]=${array[$i]}
+  array[$i]=$tmp
+done
+
+for (( i = 0; i < $length; i++)); do
+  echo -n ${array[$i]}
+  echo -n " "
+done
+echo ""
+
 
 exit 0
