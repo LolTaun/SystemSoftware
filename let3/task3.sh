74921cc7f2d8c1db794b2ff398a9c8f7304251b0
LolTaun <loltaun001@gmail.com>
2022-11-08 23:39:47 +0300

    let3 task3 fixed

--- let3/task3.sh
+++ let3/task3.sh
@@ -6,31 +6,58 @@ if ! [[ $1 =~ $re ]]; then
   echo "WRONG INPUT, must be only numbers, while yours is $1"
   exit 1
 fi
-
+# assigning helpful variable
+number_of_zeros=0
+found_digit=0
 # transferring input into an array
 while read -n 1 c; do
   array+=($c)
 done <<<$1
 
-# loop to find permutation indexes (from and where to insert)
-for ((index = 0; index < ${#array[@]}; index++)); do
-  smallest_index=$index
-  for ((i = $(($index + 1)); i < ${#array[@]}; i++)); do
-    if ((${array[$i]} < ${array[$smallest_index]})); then
-      smallest_index=$i
+# loop to find special numbers with 2 or more '0' at the start
+for ((i = 0; i < ${#array[@]}; i++)); do
+  if((!$found_digit)); then # when the first digit (except of '0') was not found
+    if(( $((${array[$i]})) != 0 )); then # if the first digit (except of '0') was found
+      found_digit=1 # set true
+      smallest_index=$i # set smallest index (temporary or finally)
     fi
-  done
-  if (($smallest_index != $index)); then
-    break
+  else # when the first digit (except of '0') was found
+    if(( $((${array[$i]})) == 0 )); then # if '0' was found after the first digit
+      number_of_zeros=$(($number_of_zeros + 1)) # increase number of found zeros
+    else break # if digit other than '0' was found
+    fi
   fi
 done
+unset found_digit # delete var
 
-if (($index != ${#array[@]})); then # if permutation is needed
+
+if(( $number_of_zeros > 1 )); then # (IN CASE WITH 2 or more '0' at the start)
+  index=$(($number_of_zeros + $smallest_index))
+  else # (IN CASE WITHOUT '0' at the start)
+    # loop to find permutation indexes (from and where to insert)
+    for ((index = 0; index < ${#array[@]}; index++)); do
+      smallest_index=$index
+      for ((i = $(($index + 1)); i < ${#array[@]}; i++)); do
+        if ((${array[$i]} < ${array[$smallest_index]})); then
+          smallest_index=$i
+        fi
+      done
+      if (($smallest_index != $index)); then
+        break
+      fi
+    done
+
+fi
+
+
+if (( $index != ${#array[@]})); then # if permutation is needed
   # creating an answer array
   result=()
   # inserting digits in a result
   for ((i = 0; i < ${#array[@]}; i++)); do
-    if (($i < $index)); then # (before insertion)
+    if(( $i >= $smallest_index && $i < $index )); then
+      result[$i]=0
+    elif (($i < $index)); then # (before insertion)
       result[$i]=${array[$i]}
     elif (($i == $index)); then # (inserted smallest digit)
       result[$i]=${array[$smallest_index]}
